0d3dd6e0762abb773a9db6c6ed19980eae51c776
rtfonline@gmail.com <rtfonline@gmail.com@940f81da-5cb3-1880-620b-c25cbe21cc74>
2013-06-13 11:33:41 +0000

    Portage sur MonoGame !

--- trunk/003 - Gestion Graphique/Gestion_Graphique/Gestion_Graphique/Properties/AssemblyInfo.cs
+++ trunk/003 - Gestion Graphique/Gestion_Graphique/Gestion_Graphique/Properties/AssemblyInfo.cs
@@ -7,22 +7,20 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Gestion_Graphique")]
 [assembly: AssemblyProduct("Gestion_Graphique")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("0f5ba58c-cb6f-49bd-b621-1228c7e6d893")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("639ec1ec-cc40-4868-984d-7321950e7425")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
