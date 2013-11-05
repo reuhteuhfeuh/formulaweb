39db95fe51d6a7cc88bc3070032206aadac26159
rtfonline@gmail.com <rtfonline@gmail.com@940f81da-5cb3-1880-620b-c25cbe21cc74>
2013-11-05 07:44:27 +0000

    Commit global avec ajout des fichiers manquants .... oublié lol

---
+++ 400 - Traceur/Traceur/Traceur.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace Traceur
+{
+    public class Traceur
+    {
+        StreamWriter Gestion_Traceur ;
+        public string level { get; set; }
+        public string destination { get; set; }
+        public bool SQL = true;
+        private static SqlConnection cnx ;
+
+        public bool Init()
+        {
+            Gestion_Traceur = new StreamWriter("FormulaWeb.log");
+            Gestion_Traceur.AutoFlush = true;
+            return true;
+        }
+
+        public bool Init(string Nom_Fichier)
+        {
+            Gestion_Traceur = new StreamWriter(Nom_Fichier);
+            Gestion_Traceur.AutoFlush = true;
+            return true;
+        }
+
+        public bool Init(string Nom_Fichier, bool BDD)
+        {
+            Init(Nom_Fichier);
+            cnx = new SqlConnection("Data Source=REUHTEUHFEUH\\FORMULAWEB;Initial Catalog=Formula_Web;Integrated Security=True;Pooling=False");
+            // Tentative connexion au serveur SQL
+            try
+            {
+                cnx.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Fweb_Log (Log_Level,Log_Commentaire,Log_Date) VALUES ('INFO','Serveur en mode ecoute',GETDATE())", cnx);
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException)
+            {
+                SQL = false;
+                Trace("INFO", "Serveur SQL Inacessible passage au log sur fichier");
+            }
+            return true;
+        }
+
+        public bool Trace(string Level, string Message)
+        {
+            if (destination == "SQL" & SQL)
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO Fweb_Log (Log_Level,Log_Commentaire,Log_Date) VALUES ("+Level+","+Message+",GETDATE())", cnx);
+                cmd.ExecuteNonQuery();
+            }
+
+            // pour le moment on garde toujours une trace sur fichier
+            Gestion_Traceur.WriteLine(DateTime.Now + " : " + Level + " : " + Message);
+            return true;
+        }
+
+    }
+}
---
+++ 600 - Serveur/FormulaWebServ/FormulaWebServ/FwebServeur.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Traceur;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace FormulaWebServ
+{
+    class FwebServeur
+    {
+        private static Int32 port_reseau = 12546;
+        private static Int32 port_param = 12590;
+        private static string Version = "0.1";
+        
+ 
+
+
+        static void Main(string[] args)
+        {           
+            Traceur.Traceur Serv_log = new Traceur.Traceur();
+            Serv_log.Init("FormulaWebServeur.log",true);
+            Serv_log.Trace("Initialisation du serveur FWeb version "+Version,"Log");
+            Thread SFWEB = new Thread(new ParameterizedThreadStart (Ecoute_Reseau));
+            Thread SFWEBPARAM = new Thread(new ParameterizedThreadStart (Ecoute_Reseau_parametrage));
+            SFWEB.Start(port_reseau);
+            SFWEBPARAM.Start(port_param);
+        }
+
+        static void Ecoute_Reseau(object port)
+        {
+            Int32 port_udp_jeu = (Int32)port;
+            Console.WriteLine("Le serveur se met en mode ecoute ... sur le port " + port_udp_jeu.ToString());
+
+            UdpClient SFWEBUDP = new UdpClient(port_udp_jeu);
+            while (true)
+            {
+
+            }
+        }
+
+        static void Ecoute_Reseau_parametrage(object port)
+        {
+            Int32 port_udp_param = (Int32)port;
+            Console.WriteLine("Le serveur parametrage se met en mode ecoute ... sur le port " + port_udp_param.ToString());
+            UdpClient SFWEBUDPPARAM = new UdpClient(port_udp_param);
+            while (true)
+            {
+
+            }
+        }
+    }
+}
