d1c2257a91420d8f241a0075a340ceb8755bd976
rtfonline@gmail.com <rtfonline@gmail.com@940f81da-5cb3-1880-620b-c25cbe21cc74>
2014-07-11 09:50:43 +0000

    Commit : Gestion Son, Gestion Reseau manquante dans le dépot
    Ajout de la Gestion_BDD pour hypothétique gestion multiple typologie de BDD.

---
+++ 006 - Gestion Son/Gestion_Son/SoundMachine.cs
@@ -0,0 +1,73 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+using Traceur;
+
+#endregion
+
+namespace Gestion_Son
+{
+    public class SoundMachine
+    {
+        ContentManager Content_Music;
+        GraphicsDeviceManager graphics_Music;
+        
+        SoundEffect Theme;
+        SoundEffect Menu_Deplacement;
+
+
+
+        Traceur.Traceur Son_Traceur;
+
+        public void Allumage_ampli(ContentManager con, GraphicsDeviceManager gdm, Traceur.Traceur tra)
+        {
+            Content_Music = con;
+            graphics_Music = gdm;
+            Son_Traceur = tra;
+        }
+
+        public bool Ecouter_musique(string Morceau)
+        {
+            MediaPlayer.Stop();
+            try
+            {
+                //Theme = Content_Music.Load<Song>(Morceau+".wma");
+                Theme = Content_Music.Load<SoundEffect>("Musique\\"+Morceau+".wav");
+            }
+            catch
+            {
+                Son_Traceur.Trace("Alerte", "Impossible de charger theme : " + Morceau);
+                return false;
+            }
+
+            var bidule = Theme.CreateInstance();
+            bidule.IsLooped = true;
+            bidule.Play();
+            //MediaPlayer.IsRepeating = true;
+            //MediaPlayer.Play(Theme);
+            
+            
+            return true;
+        }
+
+        public bool Jouer_effet(string Effet)
+        {
+            try
+            {
+                Menu_Deplacement = Content_Music.Load<SoundEffect>(Effet);
+            }
+            catch
+            {
+                Son_Traceur.Trace("Alerte", "Impossible de charger effet : " + Effet);
+                return false;
+            }
+
+            Menu_Deplacement.Play();
+            return true;
+        }
+    }
+}
---
+++ 700 - Gestion_BDD/Gestion_BDD/GestionBdd.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Traceur;
+
+namespace Gestion_BDD
+{
+    public class GestionBdd
+    {
+        private String Type_DD {get;set;}
+
+        private static SqlConnection cnx;
+
+        Traceur.Traceur Bdd_log;
+
+        public bool Initialisation(string typo)
+        {
+            Bdd_log = new Traceur.Traceur();
+            Bdd_log.Init("FormulaWebBdd.log", true);
+            Bdd_log.Trace("Initialisation du serveur Bdd", "Log");
+
+            
+
+            // on initialise la connexion BDD en fonction du type
+            switch (Type_DD)
+            {
+                case "SQLSERVEUR" :
+
+                case "MYSQL" :
+
+                case "MONGODB" :
+
+                default : 
+                    break ;
+
+
+            }
+
+            return true;
+        }
+
+            
+
+            
+            // connexion au serveur SQL
+            /*
+            cnx = new SqlConnection("Data Source=REUHTEUHFEUH\\FORMULAWEB;Initial Catalog=Formula_Web;Integrated Security=True;Pooling=False");
+            try
+            {
+                //cnx.Open();
+            }
+            catch (SqlException)
+            {
+
+                Bdd_log.Trace("INFO", "Serveur SQL Inacessible depuis le serveur du jeu");
+            }*/
+
+        }
+
+        
+    }
+}
