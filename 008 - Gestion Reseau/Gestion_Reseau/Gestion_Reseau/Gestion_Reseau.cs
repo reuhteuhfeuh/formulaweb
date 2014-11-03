cfbb9650f93c8c60484732083e0149c70ab46d25
rtfonline@gmail.com <rtfonline@gmail.com@940f81da-5cb3-1880-620b-c25cbe21cc74>
2014-11-03 08:28:12 +0000

    FormulaWeb Serveur : Début de la constitution de la structure de fonctionnement : - Gestion_Metier.cs : Gestion de tous les accès base de données - Gestion_Partie.cs : Instancié par thread de partie
    La Gestion_Métier devra également être utilisé par le site Web. C'est ici que l'on par exemple : 
    - créer une partie sur le serveur de jeu
    - Modifier les données de la partie
    - ...

--- 008 - Gestion Reseau/Gestion_Reseau/Gestion_Reseau/Gestion_Reseau.cs
+++ 008 - Gestion Reseau/Gestion_Reseau/Gestion_Reseau/Gestion_Reseau.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
@@ -15,12 +16,14 @@ namespace Gestion_Reseau
         private Int16 PortJeu;
         TcpClient ClientJeu;
         NetworkStream Message;
+        static private Boolean Connecte;
         public Traceur.Traceur tracereseau { get; set; }
         public Boolean Initialisation()
         {
             tracereseau.Trace("INFO", "Initialisation Gestion Reseau");
             ServeurJeu = "Localhost";
             PortJeu = 12546;
+            Connecte = true;
 
             try
             {
@@ -28,6 +31,9 @@ namespace Gestion_Reseau
                 Message = ClientJeu.GetStream();
                 Byte[] data = System.Text.Encoding.ASCII.GetBytes("PREMIERE CONNEXION !!!!");
                 Message.Write(data, 0, data.Length);
+                Thread Reseau = new Thread(Gestion_Reseau);
+                Reseau.Start();
+                tracereseau.Trace("INFO", "Thread Reseau lancé");
                 return true;
             }
             catch
@@ -36,12 +42,18 @@ namespace Gestion_Reseau
             }
         }
 
+        static void Gestion_Reseau(object port)
+        {
+            while (Connecte)
+            {
+                // gestion des echanges avec le serveur
+            }
+        }
+
         public void Fermeture()
         {
             Byte[] data = System.Text.Encoding.ASCII.GetBytes("DECONNEXION");
             Message.Write(data, 0, data.Length);
-           
-            //ClientJeu.
         }
 
         public void Envoi_Message(string msg)
